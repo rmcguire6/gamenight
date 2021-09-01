9bc65e3d4823d042d09343ade2ba45d359e1cfc3
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-09-01 16:45:42 -0700

    feat: Add dynamic page links

--- api/api.py
+++ api/api.py
@@ -3,12 +3,12 @@ from flask import render_template
 
 api = Flask(__name__)
 
-rooms=[{'title': 'The Perils of Markenburg','text': 'You stand at the opening of the Fissure of Markenburg. You must choose what you will do.', 'room_id': 0,'choice_one': 'Go to the left', 'choice_two': 'Go forward', 'choice_three': 'Go to the right', 'monsters': None },
-{'title': 'A Cavern','text': 'You stand at the opening of a cavern. You must choose what you will do.', 'room_id': 1,'choice_one': 'Go back', 'choice_two': 'Go in', 'choice_three': 'Go to the right', 'monsters':['giant', 'dragon', 'spider', 'troll']},
-{'title': 'The Swamp of Markenburg','text': 'You stand at the beginning of the Swamp of Markenburg. You must choose what you will do.', 'room_id': 2,'choice_one': 'Go to the left', 'choice_two': 'Swim forward', 'choice_three': 'Go to the right', 'monsters':['giant carp', 'mermaid', 'giant stinging eel', 'water leeches']},
-{'title': 'The Forest of Markenburg','text': 'You stand at the opening of the Forest of Markenburg. You must choose what you will do.', 'room_id': 3,'choice_one': 'Go to the left', 'choice_two': 'Go forward', 'choice_three': 'Go to the right', 'monsters':['giant', 'dragon', 'spider', 'werewolf']},
-{'title': 'The Mountain of Markenburg','text': 'You stand at the side of the Mountain of Markenburg. You must choose what you will do.', 'room_id': 4,'choice_one': 'Go to the left', 'choice_two': 'Climb up', 'choice_three': 'Go to the right', 'monsters':['giant', 'dragon','werewolf', 'troll']},
-{'title': 'The River of Markenburg','text': 'You stand at the side of the River of Markenburg. You must choose what you will do.', 'room_id': 5,'choice_one': 'Go to the left', 'choice_two': 'Swim forward', 'choice_three': 'Climb the cliff on the right ','monsters':['giant carp', 'mermaid', 'giant stinging eel', 'river nymphs']}]
+rooms=[{'title': 'The Perils of Markenburg','text': 'You stand at the opening of the Fissure of Markenburg. You must choose what you will do.', 'room_id': 0,'left': 3, 'forward': 5, 'right': 1, 'monsters': None },
+{'title': 'A Cavern','text': 'You stand in a  cavern. You must choose what you will do.', 'room_id': 1,'left': 5, 'forward': 4, 'right': 1, 'monsters':['giant', 'dragon', 'spider', 'troll']},
+{'title': 'The Swamp of Markenburg','text': 'You stand at the swamp of Markenburg. You must choose what you will do.', 'room_id': 2,'left': 1, 'forward': 2, 'right': 4, 'monsters':['giant carp', 'mermaid', 'giant stinging eel', 'water leeches']},
+{'title': 'The Forest of Markenburg','text': 'You stand in the forest of Markenburg. You must choose what you will do.', 'room_id': 3,'left': 4, 'forward': 3, 'right': 5, 'monsters':['giant', 'dragon', 'spider', 'werewolf']},
+{'title': 'The Mountain of Markenburg','text': 'You stand  in the mountains of Markenburg. You must choose what you will do.', 'room_id': 4,'left': 5, 'forward': 2, 'right': 4, 'monsters':['giant', 'dragon','werewolf', 'troll']},
+{'title': 'The River of Markenburg','text': 'You stand at the river of Markenburg. You must choose what you will do.', 'room_id': 5,'left': 3, 'forward': 2, 'right':  1,'monsters':['giant carp', 'mermaid', 'giant stinging eel', 'river nymphs']}]
 
 
 @api.route("/")
@@ -17,7 +17,7 @@ def start_page():
     game_message='Please start'
     return render_template('page.html', room=room, game_message=game_message)
 
-@api.route("/<int:room_id>")
+@api.route("/room/<int:room_id>")
 def build_room(room_id):
     room=rooms[room_id]
     game_message='Please continue'
