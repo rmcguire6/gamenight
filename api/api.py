9885eb72bf428ee49fab8f1fd72365a5b223f5b3
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-08-31 20:14:45 -0700

    refactor: Refactor page to room

--- api/api.py
+++ api/api.py
@@ -5,5 +5,5 @@ api = Flask(__name__)
 
 @api.route("/")
 def start_page():
-    page={'title': 'The Fissure of Markenburg', 'text':'You must choose what you will do.', 'choice_one': 'Go to the left', 'choice_two': 'Go forward', 'choice_three': 'Go to the right'}
-    return render_template('page.html', page=page)
+    room={'title': 'The Perils of Markenburg','text': 'You stand at the opening of the Fissure of Markenburg. You must choose what you will do.', 'room_id': 0,'choice_one': 'Go to the left', 'choice_two': 'Go forward', 'choice_three': 'Go to the right'}
+    return render_template('page.html', room=room)
